876836b977ed7eac4a7066f561736e3f71ff7d0f
tgeveke <tgeveke1@gmail.com>
2022-12-11 22:21:05 -0500

    Working app

--- RobotClass.py
+++ RobotClass.py
@@ -2,9 +2,7 @@ class Robot():
     import sys
     from time import sleep
     from xarm.wrapper import XArmAPI
-    from CameraClass import Camera
-
-    def __init__(self, ip='192.168.1.207', speed = 125):
+    def __init__(self, ip='192.168.1.207', speed=500):
         print('Robot class __init__()')
 
         self.arm = self.XArmAPI(ip)
@@ -13,62 +11,68 @@ class Robot():
         self.locations = {
             # [x, y, z, roll, pitch, yaw]
             'frontPrinter': [50, 400, 50, -90, 90, 90],
-            'topPrinter': [-100, 400, 250, -180, 0, 0]
+            'topPrinter': [-220, 465, 235, -180, 0, 0],
+            'dropOff': [5, 330, 10, 0, 0, 0]
         }
-
-    def grab(self, startPos = 800, endPos = 150):
+        self.reset()
         self.arm.set_gripper_enable(True)
-        self.arm.set_gripper_position(startPos, wait=False)
-        self.arm.set_servo_angle(servo_id=6, angle=190, speed=50, is_radian=False, wait=True)
-        self.arm.set_position(z=-50, relative=True)
-        self.sleep(1)
-        self.arm.set_gripper_position(endPos, wait=True)  
 
-    def move2target(self, x, y, z, angle):
-        self.arm.set_position(x=x, y=y, z=z, roll=angle, speed=self.global_speed, wait=True)
 
-        distance = 125  # getDistance()
-        if 0 < distance < 200:  # Reasonable values
-            print('Distance to target:', distance)
-        else:
-            print('Error with distance:', distance)
-            exit()
+    def move2target(self, x, y, z):
+        self.arm.set_position(x=x, y=y, z=z, relative=False, speed=self.global_speed/3, wait=True)
+
+    def grab(self, startPos=800, endPos=150, angle=0):
+        self.arm.set_servo_angle(servo_id=6, angle=angle, relative=True, is_radian=False, speed=50, wait=True)
+        self.arm.set_position(z=-50, relative=True, speed=50, wait=True)
+        self.arm.set_gripper_position(endPos, wait=True)
+
+    def dropOff(self):
+        '''
+        location_values = self.locations['dropOff']
+        [x_i, y_i, z_i, roll_i, pitch_i, yaw_i] = location_values
+
+        self.arm.set_position(x=x_i, y=y_i, z=z_i, roll=roll_i, pitch=pitch_i, yaw=yaw_i, speed=self.global_speed,
+                              wait=True)
+        '''
+        location_values = self.locations['dropOff']
+        [x_i, y_i, z_i, roll_i, pitch_i, yaw_i] = location_values
+
+        self.arm.set_position(z=+100, relative=True, speed=self.global_speed/3, wait=True)
+        self.arm.set_position(x=x_i, y=y_i, z=z_i, yaw=yaw_i, speed=self.global_speed/3,
+                              wait=True)
+        self.arm.set_gripper_position(800, wait=False)
+        self.arm.set_position(z=+100, relative=True, speed=self.global_speed/3, wait=True)
 
     def view_frontPrinter(self):
-        self.setPlane('front')
-
         location_values = self.locations['frontPrinter']
         [x_i, y_i, z_i, roll_i, pitch_i, yaw_i] = location_values
-        self.arm.set_position(x=x_i, y=y_i, z=z_i, roll=roll_i, pitch=pitch_i, yaw=yaw_i, speed=self.global_speed, wait=True)
+        self.arm.set_position(x=x_i, y=y_i, z=z_i, roll=roll_i, pitch=pitch_i, yaw=yaw_i, speed=self.global_speed,
+                              wait=True)
 
     def view_topPrinter(self):
-        self.setPlane('top')
-
         location_values = self.locations['topPrinter']
         [x_i, y_i, z_i, roll_i, pitch_i, yaw_i] = location_values
-        self.arm.set_position(x=x_i, y=y_i, z=z_i, roll=roll_i, pitch=pitch_i, yaw=yaw_i, speed=self.global_speed, wait=True)
+        self.arm.set_position(y=y_i, relative=True, speed=self.global_speed, wait=True)
+        self.arm.set_position(z=z_i, relative=True, speed=self.global_speed, wait=True)
+        self.arm.set_position(x=x_i, y=y_i, z=z_i, roll=roll_i, pitch=pitch_i, yaw=yaw_i, speed=self.global_speed,
+                              wait=True)
 
-    def viewObjectTop(self):
-        self.setPlane('top')
-
-    def setPlane(self, current_plane):
-        self.plane = current_plane
-
-    def wait(self, time = 10):
+    def wait(self, time=10):
         self.sleep(time)
 
-    def home(self, speed = 50):
-        self.arm.move_gohome(speed)
+    def home(self, speed=100):
+        self.arm.move_gohome(speed=speed)
 
     def reset(self):
         self.arm.clean_warn()
         self.arm.clean_gripper_error()
         self.arm.clean_error()
+        self.sleep(1)
         self.arm.reset()
 
     def disconnect(self):
         self.arm.disconnect()
 
+
 if __name__ == '__main__':
     obj = Robot()
-    obj.reset()
--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 class app():
-    def __init__(self, robot=True, camera=True, gcode=True, gcodeFileName = r'TestData\GCode\bency-gcode.gcode', bbox=True, ml=True, printer=True):
+    def __init__(self, robot=True, camera=True, gcode=True, gcodeFileName='TestData/GCode/bency-gcode.gcode', bbox=True,
+                 ml=True, printer=True):
         import sys
         if robot:
             from RobotClass import Robot
             self.Robot = Robot()
         if camera:
             from CameraClass import Camera
-            self.Camera = Camera(save=False)
+            self.Camera = Camera(save=True, stream=False, delay=10)
         if gcode:
             from GCodeParserClass import GCodeParser
             self.GCodeParser = GCodeParser(fileName=gcodeFileName)
@@ -22,84 +23,113 @@ class app():
             from PrinterClass import Printer
             self.Printer = Printer()
 
-    def captureImage(self):
-        return self.Camera.capture()  # rgb_array, depth_array
+    def captureImage(self, save):
+        return self.Camera.capture(save=save)  # rgb_array, depth_array
 
     def clusterGCode(self):
         clusters = self.GCodeParser.cluster2D()
         print(clusters[1])
-    
+
     def avgGCode(self):
         avgs = self.GCodeParser.averageXYZ()
         return avgs
-    
+
     def getBoundingBoxes(self, img, plot=True):
-        return self.BoundingBox.findBBoxes(img, plot)
-    
-    def xyangle_fromBBox(self, rectangle):
-        return rectangle[0]
-    
-    def moveToLocation(self, x, y, z, angle):
-        self.Robot.move2target(x, y, z, angle)
+        return self.BoundingBox.findBBoxes(img=img, plot=plot)
+
+    def moveToLocation(self, x, y, z):
+        self.Robot.move2target(x, y, z)
+
+    def grab2droppoff(self, width, angle):
+        self.Robot.grab(endPos=width, angle=angle)
+        self.Robot.dropOff()
+
+    def printer2robot(self, pos, imgsize):
+        cam_x, cam_y, cam_z = pos
+
+        scale = 0.4
+        imglength = imgsize[0]
+        imgheight = imgsize[1]
+
+        relx = -295 + (scale * (cam_y - (imgheight / 2)))
+        rely = 410 + (scale * (cam_x - (imglength / 2)))
+        zoffset = 110
+
+        return [relx, rely, cam_z + zoffset]
 
     def getMLoutput(self, imgName='TestData\Images\sample_6b26ca74_1_color.jpg'):
-        output = self.model.predict(imgName, confidence=40, overlap=30).json()['predictions'] #[0]
+        output = self.model.predict(imgName, confidence=40, overlap=30).json()['predictions']  # [0]
         if len(output) > 0:
             # Get values from model
             x = output[0]['x']
             y = output[0]['y']
-
             return [x, y]
         else:
             print('No prediction made')
             return
 
-    def readInstructions(self, filename = 'instructions.txt'):
-        options = {
-            'wait': wait,
-            'reset': reset,
-            'go home': home,
-            'view front printer': view_frontPrinter,
-            'view top printer': view_topPrinter,
-            'capture image': captureImage,
-            'move to target position': move2target,
-            'grab': grab,
-            'view object top': viewObjectTop,
-            'get front distance': getFrontDistance
-        }
-
-        args = self.sys.argv()
-        print(args)
-        if len(args) > 1:
-            fileName = args[1]
-            try:
-                file = open(filename)
-            except:
-                print('Error opening file:', fileName)
-        for instruction in file.readlines():
-            if '#' not in instruction:  # Skips 'commented out' lines of instruction
-                print(instruction)
-                options[instruction.strip()]()
 
 if __name__ == '__main__':
     print('Running App')
-    obj = app(robot=True, camera=False, gcode=False, bbox=False, ml=False, printer=False)
+    obj = app(robot=True, camera=True, gcode=True, bbox=True, ml=True, printer=False)
+    obj.Robot.view_topPrinter()
 
-    # imgs = obj.captureImage()
-    # colorImg = imgs[0]
-    # imgname = imgs[2]
+    imgs = obj.captureImage(save=True)
+    colorImg = imgs[0]
+    imgname = imgs[2]
+    print('Saved image at:', imgname)
 
-    imgname = 'sample_a3d90371\sample_a3d90371_0_color.jpg'
+    imgwidth = len(colorImg[0])
+    imglength = len(colorImg)
+    print('Image Size =', imgwidth, imglength)
 
-    # MLoutput = obj.getMLoutput(imgName=imgname)
-    # print(MLoutput)
-
-    #colorImg = obj.BoundingBox.openImg(imgname)
-    #bbox = obj.getBoundingBoxes(colorImg, plot=True)
-    #for rectangle in bbox:
-        # xyang_fromBBox = obj.xyangle_fromBBox(rectangle)
-        # print(rectangle)
-    
-    # GCode_avgXYZ = obj.avgGCode()
+    GCode_avgXYZ = obj.avgGCode()
     # print(GCode_avgXYZ)
+    GCodex = GCode_avgXYZ[0]
+    GCodey = GCode_avgXYZ[1]
+    GCodez = GCode_avgXYZ[2]
 
+    MLoutput = obj.getMLoutput(imgName=imgname)
+    while MLoutput is None:
+        print('ML error')
+        imgs = obj.captureImage(save=True)
+        colorImg = imgs[0]
+        imgname = imgs[2]
+        print('Saved image at:', imgname)
+        MLoutput = obj.getMLoutput(imgName=imgname)
+
+    print('Machine Learning Output (x, y):', MLoutput)
+    mlx = MLoutput[0]
+    mly = MLoutput[1]
+
+    bbox = obj.getBoundingBoxes(colorImg, plot=False)
+    while len(bbox) == 0:
+        bbox = obj.getBoundingBoxes(colorImg, plot=False)
+
+    thresh = 15
+    for rectangle in bbox:
+        xy = rectangle[0]
+        print(xy)
+        x = xy[0]
+        y = xy[1]
+        if int(x) in range(int(mlx) - thresh, int(mlx) + thresh) and int(y) in range(int(mly) - thresh,
+                                                                                     int(mly) + thresh):
+            print('Found matching point')
+            angle = round(rectangle[2], 2)
+            print('Angle =', angle)
+
+            scale = 4
+            widths = rectangle[1]
+            if scale * min(widths) > 800:
+                print('Part too large')
+            else:
+                width = scale * min(widths)
+                print('Width =', width)
+
+            [x, y, z] = obj.printer2robot(pos=[mlx, mly, 10], imgsize=[imgwidth, imglength])
+            print(x, y, z)
+            if int(x) in range(-350, -150) and int(y) in range(350, 500):
+                obj.moveToLocation(x=x, y=y, z=z)
+                obj.grab2droppoff(width=width, angle=90 + angle)
+            obj.Robot.home()
+            exit()
